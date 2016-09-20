7ece1709464d33b25622485659b76efad2776ddd
Ari Neto <ari.oliveira@gmail.com>
2016-09-20 12:28:08 -0300

    Adding ds_list.py - tested on 9.0

--- ds_list.py
+++ ds_list.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0,'./deep-security-py')
 import deepsecurity
 
-mgr = deepsecurity.dsm.Manager(hostname='127.0.0.1', username='user', password='TrendMicro2016!', ignore_ssl_validation=True)
+mgr = deepsecurity.dsm.Manager(hostname='127.0.0.1', port='4119', username='user', password='TrendMicro2016!', ignore_ssl_validation=True)
 mgr.sign_in()
 mgr.computers.get()
 
@@ -21,6 +21,13 @@ try:
         overall_intrusion_prevention_status = computer.overall_intrusion_prevention_status
         overall_web_reputation_status = computer.overall_web_reputation_status
         overall_log_inspection_status = computer.overall_log_inspection_status
+        anti_malware_classic_pattern_version = computer.anti_malware_classic_pattern_version
+        anti_malware_engine_version = computer.anti_malware_engine_version
+        anti_malware_intelli_trap_exception_version = computer.anti_malware_intelli_trap_exception_version
+        anti_malware_intelli_trap_version = computer.anti_malware_intelli_trap_version
+        anti_malware_smart_scan_pattern_version = computer.anti_malware_smart_scan_pattern_version
+        anti_malware_spyware_pattern_version = computer.anti_malware_spyware_pattern_version
+        last_anti_malware_event = computer.last_anti_malware_event
         print ("Computer_ID: %s" % computer_id)
         print ("Computer Name: %s" % name)
         print ("Platform: %s" % platform )
@@ -28,13 +35,17 @@ try:
         print ("Overall Status: %s" % overall_status)
         print ("Overal Version: %s" % overall_version)
         print ("Overall Anti Malware Status: %s" % overall_anti_malware_status)
+        # print ("Overall Anti Malware Classic Pattern Vr: %s" % anti_malware_classic_pattern_version)
+        # print ("Overall Anti Malware Engine Vr: %s" % anti_malware_engine_version)
+        # print ("Overall Anti Malware Intelli Trap Exception Vr: %s" % anti_malware_intelli_trap_exception_version)
+        print ("Overall Anti Malware Intelli Trap Vr: %s" % anti_malware_intelli_trap_version)
+        print ("Overall Anti Malware Smart Scan Pattern Vr: %s" % anti_malware_smart_scan_pattern_version)
+        # print ("Overall Anti Malware Spyware Pattern Vr: %s" % anti_malware_spyware_pattern_version)
+        print ("Last Anti Malware Event: %s" %  last_anti_malware_event)
         print ("Overall Anti Firewall Status: %s" % overall_firewall_status)
         print ("Overall Anti IPS Status: %s" % overall_intrusion_prevention_status)
         print ("Overall Web Reputation Status: %s" % overall_web_reputation_status)
         print ("Overall Log Inspection Status: %s" % overall_log_inspection_status)
-        print ("Clear Alerts and Warnings: %s" % computer.clear_alerts_and_warnings())
-        print ("Get Recommended Rules: %s" % computer.get_recommended_rules())
-        computer.send_events()
 except Exception as e:
     print ("Error %s" % e)
     mgr.sign_out()
